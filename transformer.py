037046dec1fe6825afb49413fca636fc430628ed
Daniel D <cloudcrypt@hotmail.com>
2015-04-14 19:41:35 -0600

    new feature

--- transformer.py
+++ transformer.py
@@ -1,3 +1,6 @@
+from pitchSet import *
+from flask import jsonify
+
 def transposeRow(integerString, row):
     transformation = getTransformation(integerString)
     result = []
@@ -78,13 +81,41 @@ def performOperation(user_operation, row):
     elif user_operation[0:2] == "RI":
         result = retrogradeInvertRow(user_operation[2:], row)
     result_string = user_operation + ":"
+    result_string_note1 = user_operation + ":"
+    result_string_note2 = user_operation + ":"
     for element in result:
+        if element == "T":
+            elementForName = 10
+        elif element == "E":
+            elementForName = 11
+        else:
+            elementForName = element
+        mapping1 = pitchSet.int_note_mappings_1[elementForName].upper()
+        mapping2 = pitchSet.int_note_mappings_2[elementForName].upper()
+        mapping1 = mapping1.replace('-', 'b')
+        mapping1 = mapping1.replace('+', '#')
+        mapping2 = mapping2.replace('-', 'b')
+        mapping2 = mapping2.replace('+', '#')
         result_string += " " + str(element)
-    return result_string
+        result_string_note1 += " " + str(mapping1)
+        result_string_note2 += " " + str(mapping2)
+    finalResult = []
+    finalResult.append(result_string)
+    finalResult.append(result_string_note1)
+    finalResult.append(result_string_note2)
+    print finalResult
+    return jsonify(list=finalResult)
 	
 	
 	
-	
+                # mapping1 = self.int_note_mappings_1[pitchInt].upper()
+                # mapping2 = self.int_note_mappings_2[pitchInt].upper()
+                # mapping1 = mapping1.replace('-', 'b')
+                # mapping1 = mapping1.replace('+', '#')
+                # mapping2 = mapping2.replace('-', 'b')
+                # mapping2 = mapping2.replace('+', '#')
+                # self.matrixNote1[matrixPosition].append(mapping1)
+                # self.matrixNote2[matrixPosition].append(mapping2)
 	
 	
 	
